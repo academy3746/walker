843284306a73ab8c324706e60e15b44fbb457a32
ijonghyeon <academy3746@gmail.com>
2023-11-08 21:34:44 +0900

    시발..ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- lib/features/widgets/health_info.dart
+++ lib/features/widgets/health_info.dart
@@ -11,7 +11,6 @@ class HealthDataFetcher {
 
   var permissions = [
     HealthDataAccess.READ_WRITE,
-    HealthDataAccess.READ_WRITE,
   ];
 
   Future<bool> requestHealthDataAuthorization() async {
